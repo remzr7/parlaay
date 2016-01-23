125f105b4ab48eef2c9056194e87f5efbff88fc7
Rameez Remsudeen <remzr7@gmail.com>
2016-01-23 14:28:39 -0500

    init

--- parlaay.py
+++ parlaay.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 import sys
+
 from docker import Client
-
-def main():
-    return 0
-
 cli = Client(base_url='tcp://45.79.170.248:2375')
 
-def createContainer:
-    
+def build():
+    print("building...")
+
+def main():
+    for arg in sys.argv[0]:
+        if arg is "info":
+            ##
+            continue
+        elif arg is "build":
+            build()
+
+    print('execed')
+    return 0
+
+main()
